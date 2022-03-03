3bfd285e0a90eac40ad136c0df82e3d04b733e52
Harshitp7 <panwar.harshit0602@gmail.com>
2022-03-03 12:39:14 +0530

    navbar scroll settings

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
-        if (scroll > 300) {
-          $(".navbar").css("background" , "#041E54");
+        if (scroll > 300 && scroll<1000) {
+          $(".navbar").css("background" , "#041d54a9");
           $(".nav-link").css("color", "#FFCB04");
+        }
+        else if(scroll > 1000){
+          $(".navbar").css("background" ,  "#041d54");
         }
         else{
             $(".navbar").css("background" , "");
+
             $(".nav-link").css("color", "white");  	
         }
     });
